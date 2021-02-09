20403e2792e2055e4e50edcd18644e5c2f1ad447
Mushfiqur Anik <43996786+mushfiqur-anik@users.noreply.github.com>
2021-02-08 23:02:52 -0500

    Added All Cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -22,6 +22,31 @@ const Cards = () => {
                         path="/services"
                      />
                   </ul>
+
+                  <ul className="cards__items"> 
+                     <CardItem 
+                        src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1955&q=80"
+                        text="Explore the hidden waterfall deep inside the Amazon Jungle"
+                        label="Adventure"
+                        path="/services"
+                     />
+                     <CardItem 
+                        src="https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+                        text="Travel through the Islands of Bali in a Private Cruise"
+                        label="Beautiful"
+                        path="/products"
+                     />
+                     <CardItem 
+                        src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+                        text="Travel through the Islands of Bali in a Private Cruise"
+                        label="Luxury"
+                        path="/products"
+                     />
+
+
+                  </ul>
+
+                 
                </div>
             </div>    
         </div>
