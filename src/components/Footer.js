54b172daf32144008914f797d842ab37b4ca3cf8
Mushfiqur Anik <43996786+mushfiqur-anik@users.noreply.github.com>
2021-02-09 03:46:57 -0500

    Final Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from "./Button"
 import "./Footer.css"
+import {Link} from "react-router-dom"
 
 const Footer = () => {
     return (
@@ -22,16 +23,99 @@ const Footer = () => {
             </section>
 
             <div className="footer-links">
+
                 <div className="footer-link-wrapper">
                     <div className="footer-link-items">
                         <h2> About US </h2>
+                        <Link to="/"> How it works  </Link>
+                        <Link to="/"> Testimonials </Link>
+                        <Link to="/"> Careers </Link>
+                        <Link to="/"> Investors </Link>
+                        <Link to="/"> Terms of Service </Link>
+                    </div>
 
+                    <div className="footer-link-items">
+                        <h2> Contact US </h2>
+                        <Link to="/"> Contact </Link>
+                        <Link to="/"> Support </Link>
+                        <Link to="/"> Destinations </Link>
+                        <Link to="/"> Sponsporships</Link>
+                    </div>
+                </div>
+
+                <div className="footer-link-wrapper">
+                    <div className="footer-link-items">
+                        <h2> Videos </h2>
+                        <Link to="/"> Submit Videos </Link>
+                        <Link to="/"> Ambassadors </Link>
+                        <Link to="/"> Agency </Link>
+                        <Link to="/"> Influencer </Link>
+                    </div>
+
+                    <div className="footer-link-items">
+                        <h2> Social Media </h2>
+                        <Link to="/"> Instagram </Link>
+                        <Link to="/"> Facebook </Link>
+                        <Link to="/"> Youtube </Link>
+                        <Link to="/"> Twitter </Link>
                     </div>
                 </div>
             </div>
 
-
-
+            <section class='social-media'>
+        <div class='social-media-wrap'>
+          <div class='footer-logo'>
+            <Link to='/' className='social-logo'>
+              TRVL
+              <i className='fab fa-typo3' />
+            </Link>
+          </div>
+          <small className='website-rights'>TRAVEL © 2020</small>
+          <div class='social-icons'>
+            <Link
+              class='social-icon-link facebook'
+              to='/'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <i class='fab fa-facebook-f' />
+            </Link>
+            <Link
+              class='social-icon-link instagram'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <i class='fab fa-instagram' />
+            </Link>
+            <Link
+              class='social-icon-link youtube'
+              to='/'
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <i class='fab fa-youtube' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <i class='fab fa-twitter' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <i class='fab fa-linkedin' />
+            </Link>
+          </div>
+        </div>
+      </section>
+            
         </div>
     )
 }
